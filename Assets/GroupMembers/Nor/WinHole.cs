1045218a5ecef909fe6d4f0094f84faafed404b2
norhady1 <norhady1@gmail.com>
2022-04-06 16:56:44 +0200

    Nor Minigame

---
+++ Assets/GroupMembers/Nor/WinHole.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinHole : MonoBehaviour
+{
+    public string ball = "Ball";
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == ball)
+        {
+            Debug.Log("WIN!");
+
+        }
+    }
+
+}
