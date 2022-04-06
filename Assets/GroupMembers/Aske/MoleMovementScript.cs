aa7cf1cfe96ad86e6160ee5f2f7fec7e999fd4b0
akaise21 <akaise21@student.aau.dk>
2022-04-06 17:25:13 +0200

    AskeMiniGame

---
+++ Assets/GroupMembers/Aske/MoleMovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoleMovementScript : MonoBehaviour
+{
+    public GameObject Mole;
+    public Transform[] spawnPoint;
+    public Text PointsText;
+    public int points=0;
+    void Start()
+    {
+        SpawnMole();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void SpawnMole()
+    {
+        Mole.transform.position = spawnPoint[Random.Range(0, spawnPoint.Length)].transform.position;
+    }
+
+    void MolePoint()
+    {
+        PointsText.text = points+"/10";
+    }
+}
