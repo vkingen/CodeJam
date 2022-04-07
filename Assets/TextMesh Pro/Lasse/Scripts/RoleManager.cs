a2b438834f282666c5aceb662bfff94aeda07548
vkingen <lassevikingkristensen@gmail.com>
2022-04-07 12:10:19 +0200

    2

---
+++ Assets/TextMesh Pro/Lasse/Scripts/RoleManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleManager : MonoBehaviour
+{
+    public static RoleManager instance;
+
+    [HideInInspector]
+    public int role; // This variable is in charge of controlling what role the player is. The role can be either 1,2,3 or 4. 
+    
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
