918d5a9a1d40629ff549194f2fe9b7f33473c7a3
norhady1 <norhady1@gmail.com>
2022-04-07 11:12:58 +0200

    start side

---
+++ Assets/GroupMembers/Nor/StartScreen/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Loading to next scene. Finding the active scene and the build index and adding 1.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
