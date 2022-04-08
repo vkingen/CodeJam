cbd3a25cdd16619a04cb1a8d97c8c1f3eec2e412
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2022-04-08 13:15:03 +0200

    Nor's Map

---
+++ Assets/GroupMembers/Marius/MiniGame/Scripts/Accelerometer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    //The rigidbody for the game object
+    private Rigidbody rigid;
+
+    public bool isFlat = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 tilt = Input.acceleration;
+        tilt.z = 0;
+
+        //makes sure that when it is flat the tilt points downwards
+        if (isFlat)
+            tilt = Quaternion.Euler(90, 0, 0)*tilt;
+
+        //Moves the rigidbody through how the phone is tiltid
+        rigid.AddForce(tilt);
+
+        
+    }
+}
---
+++ Assets/GroupMembers/Marius/MiniGame/Scripts/WinHole.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinHole : MonoBehaviour
+{
+    public int index;
+    public string ball = "Ball";
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == ball)
+        {
+            MainSceneManager.instance.LoadNextScene(index);
+
+        }
+    }
+
+}
